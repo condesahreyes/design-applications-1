a11397c5cb96aa1feff8b1b36c6d7fedd0c19cc9
Rodrigo Haiache <rodrigohi_15@hotmail.com>
2021-04-13 21:19:02 -0300

    Implementacion de clase test/alta,baja,modificacion de Tarjeta.Primer paso TDD (RED) finalizada

---
+++ OblDiseño1/OblDiseño1/TarjetaIncorrectaException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace OblDiseño1
+{
+    [Serializable]
+    public class TarjetaIncorrectaException : Exception
+    {
+        public TarjetaIncorrectaException()
+        {
+        }
+
+        public TarjetaIncorrectaException(string message) : base(message)
+        {
+        }
+
+        public TarjetaIncorrectaException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected TarjetaIncorrectaException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- OblDiseño1/Pruebas/TarjetaTest.cs
+++ OblDiseño1/Pruebas/TarjetaTest.cs
@@ -18,6 +18,15 @@ namespace Pruebas
 
 
 
+
+        [TestMethod]
+        public void Crear_Tarjeta_Test() 
+        {
+            Tarjeta tarjetaCreada = new Tarjeta();
+            Assert.IsNotNull(tarjetaCreada);
+        }
+
+
         [TestMethod]
         public void Alta_Tarjeta_Test()
         {
@@ -25,27 +34,76 @@ namespace Pruebas
         }
 
         [TestMethod]
-        [ExpectedException(typeof(TarjetaNumeroIncorrectoException))]
+        [ExpectedException(typeof(TarjetaIncorrectaException))]
         public void Alta_Tarjeta_NumeroIncorrecto_Test() 
         {
             Tarjeta tarjetaNumeroIncoreccto = new Tarjeta().altaTarjeta(nombre, tipo, 8371635461, codigoSeguridad, fechaVencimiento, categoria,"");
             
         }
 
-
+        [TestMethod]
         public void Modificar_Tarjeta_Test() 
         {
-            Tarjeta tarjetaResultante = new Tarjeta("Hernan","MasterCard",8471998674553411,789, new DateTime(2024, 10, 05),"Trabajo","");
+            Tarjeta tarjetaResultante = new Tarjeta();
+            tarjetaResultante.altaTarjeta("Hernan","MasterCard",8471998674553411,789, new DateTime(2024, 10, 05),"Trabajo","");
             tarjeta.modificarTarjeta(nombre, tipo, numero, codigoSeguridad, fechaVencimiento, categoria, "");
             Assert.AreEqual(tarjetaEsperada, tarjetaResultante);
         }
 
+        [TestMethod]
         public void Baja_Tarjeta_Test() 
         {
-            Tarjeta tarjetaABorrar = new Tarjeta(nombre, tipo, numero, codigoSeguridad, fechaVencimiento, categoria, "");
+            Tarjeta tarjetaABorrar = new Tarjeta().altaTarjeta(nombre, tipo, numero, codigoSeguridad, fechaVencimiento, categoria, "");
             tarjetaABorrar.bajaTarjeta();
             Assert.IsNull(tarjetaABorrar);
         }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(TarjetaIncorrectaException))]
+        public void Alta_Tarjeta_CodigoSeguridadIncorrecto_Test()
+        {
+            Tarjeta tarjetaEsperada = new Tarjeta().altaTarjeta(nombre, tipo, numero, 23834, fechaVencimiento, categoria, "");
+
+        }
+
+
+
+        [TestMethod]
+        [ExpectedException(typeof(TarjetaIncorrectaException))]
+        public void Alta_Tarjeta_NombreNulo_Test()
+        {
+            Tarjeta tarjetaEsperada = new Tarjeta().altaTarjeta(null, tipo, numero, codigoSeguridad, fechaVencimiento, categoria, "");
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TarjetaIncorrectaException))]
+        public void Alta_Tarjeta_TipoNulo_Test()
+        {
+            Tarjeta tarjetaEsperada = new Tarjeta().altaTarjeta(nombre, null, numero, codigoSeguridad, fechaVencimiento, categoria, "");
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TarjetaIncorrectaException))]
+        public void Alta_Tarjeta_CodigoSeguridadNulo_Test()
+        {
+            Tarjeta tarjetaEsperada = new Tarjeta().altaTarjeta(nombre, tipo, numero, null, fechaVencimiento, categoria, "");
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TarjetaIncorrectaException))]
+        public void Alta_Tarjeta_fechaVencimientoNulo_Test()
+        {
+            Tarjeta tarjetaEsperada = new Tarjeta().altaTarjeta(nombre, tipo, numero, codigoSeguridad, null, categoria, "");
+
+        }
+
+        
+
+
 
 
 
