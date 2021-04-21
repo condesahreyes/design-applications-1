347934f88e794de14a0a69e1510051bcddba1ed8
Hernán Reyes <hreyes@Artech.local>
2021-04-21 15:11:23 -0300

    Implementación de clase SistemaTest, funcionalidad multiples usuarios. Primer paso de TDD (RED)

---
+++ OblDiseño1/Pruebas/SistemaTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OblDiseño1;
+
+namespace Pruebas
+{
+    [TestClass]
+    public class SistemaTest
+    {
+
+        private Sistema sistema;
+
+        private static string[] contrasenias = {"contrasenia123",
+            "1234Contrasenia", "laContraseña"};
+
+        private static string[] nombres = { "Hernán", "Santiago", "Rodrigo" };
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.sistema = new Sistema();
+        }
+
+        [TestMethod]
+        public void AgregarUsuariosNuevos()
+        {
+            List<Usuario> listaUsuarios = new List<Usuario>();
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Usuario usuario = new Usuario(nombres[i], contrasenias[i]);
+                sistema.AgregarUsuario(usuario);
+                listaUsuarios.Add(usuario);
+            }
+
+            CollectionAssert.AreEquivalent(listaUsuarios, sistema.ObtenerUsuarios);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ExepcionObjetosRepetidos))]
+        public void IntentarAgregarUsuariosRepetidos()
+        {
+            string nombreAux = nombres[0];
+
+            Usuario usuario1 = new Usuario(nombres[0], contrasenias[0]);
+            Usuario usuario2 = new Usuario(nombreAux, contrasenias[1]);
+
+            sistema.AgregarUsuario(usuario1);
+            sistema.AgregarUsuario(usuario2);
+        }
+
+
+    }
+}
