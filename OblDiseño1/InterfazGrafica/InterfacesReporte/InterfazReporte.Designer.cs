cedc08c7b160b6bf079c0eea50a68661c8c294a3
SantiagoBolumburu76 <santiagobolumburu@hotmail.com>
2021-05-04 17:28:09 -0300

    Avences en la implementacion de la Interfaz de reporte

---
+++ OblDiseño1/InterfazGrafica/InterfacesReporte/InterfazReporte.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace InterfazGrafica.InterfacesReporte
+{
+    partial class InterfazReporte
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_Titulo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button_Aceptar = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label_TituloGrupo = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label_TituloCantidadDeContrasenias = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_Titulo
+            // 
+            this.label_Titulo.AutoSize = true;
+            this.label_Titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Titulo.Location = new System.Drawing.Point(12, 9);
+            this.label_Titulo.Name = "label_Titulo";
+            this.label_Titulo.Size = new System.Drawing.Size(417, 29);
+            this.label_Titulo.TabIndex = 0;
+            this.label_Titulo.Text = "Reporte de Fortalesa de Contreseñas";
+            this.label_Titulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.button_Aceptar);
+            this.panel1.Location = new System.Drawing.Point(12, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(635, 399);
+            this.panel1.TabIndex = 1;
+            // 
+            // button_Aceptar
+            // 
+            this.button_Aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Aceptar.Location = new System.Drawing.Point(527, 353);
+            this.button_Aceptar.Name = "button_Aceptar";
+            this.button_Aceptar.Size = new System.Drawing.Size(102, 39);
+            this.button_Aceptar.TabIndex = 0;
+            this.button_Aceptar.Text = "Aceptar";
+            this.button_Aceptar.UseVisualStyleBackColor = true;
+            this.button_Aceptar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 42.30146F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 57.69854F));
+            this.tableLayoutPanel1.Controls.Add(this.panel5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.33334F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(617, 344);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label_TituloGrupo
+            // 
+            this.label_TituloGrupo.AutoSize = true;
+            this.label_TituloGrupo.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_TituloGrupo.Location = new System.Drawing.Point(3, 3);
+            this.label_TituloGrupo.Name = "label_TituloGrupo";
+            this.label_TituloGrupo.Size = new System.Drawing.Size(96, 33);
+            this.label_TituloGrupo.TabIndex = 0;
+            this.label_TituloGrupo.Text = "Grupo";
+            this.label_TituloGrupo.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label_TituloGrupo);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(255, 51);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label_TituloCantidadDeContrasenias);
+            this.panel3.Location = new System.Drawing.Point(264, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(350, 51);
+            this.panel3.TabIndex = 2;
+            // 
+            // label_TituloCantidadDeContrasenias
+            // 
+            this.label_TituloCantidadDeContrasenias.AutoSize = true;
+            this.label_TituloCantidadDeContrasenias.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_TituloCantidadDeContrasenias.Location = new System.Drawing.Point(3, 7);
+            this.label_TituloCantidadDeContrasenias.Name = "label_TituloCantidadDeContrasenias";
+            this.label_TituloCantidadDeContrasenias.Size = new System.Drawing.Size(281, 29);
+            this.label_TituloCantidadDeContrasenias.TabIndex = 0;
+            this.label_TituloCantidadDeContrasenias.Text = "CantidadDeContrasenias";
+            this.label_TituloCantidadDeContrasenias.Click += new System.EventHandler(this.label_TituloCantidadDeContrasenias_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel10);
+            this.panel4.Controls.Add(this.panel9);
+            this.panel4.Controls.Add(this.panel8);
+            this.panel4.Controls.Add(this.panel7);
+            this.panel4.Controls.Add(this.panel6);
+            this.panel4.Location = new System.Drawing.Point(3, 60);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(255, 281);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(264, 60);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(350, 50);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.AutoSize = true;
+            this.panel6.Controls.Add(this.label1);
+            this.panel6.Location = new System.Drawing.Point(0, 1);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(252, 50);
+            this.panel6.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.AutoSize = true;
+            this.panel7.Location = new System.Drawing.Point(0, 57);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(252, 50);
+            this.panel7.TabIndex = 1;
+            this.panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.panel7_Paint);
+            // 
+            // panel8
+            // 
+            this.panel8.AutoSize = true;
+            this.panel8.Location = new System.Drawing.Point(0, 113);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(252, 50);
+            this.panel8.TabIndex = 2;
+            // 
+            // panel9
+            // 
+            this.panel9.AutoSize = true;
+            this.panel9.Location = new System.Drawing.Point(0, 169);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(252, 50);
+            this.panel9.TabIndex = 1;
+            // 
+            // panel10
+            // 
+            this.panel10.AutoSize = true;
+            this.panel10.Location = new System.Drawing.Point(0, 225);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(252, 50);
+            this.panel10.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(100, 0, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rojo";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // InterfazReporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 477);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label_Titulo);
+            this.Name = "InterfazReporte";
+            this.Text = "InterfazReporte";
+            this.Load += new System.EventHandler(this.InterfazReporte_Load);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Titulo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_Aceptar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label_TituloGrupo;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label_TituloCantidadDeContrasenias;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ OblDiseño1/InterfazGrafica/InterfacesReporte/InterfazReporte.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfazGrafica.InterfacesReporte
+{
+    public partial class InterfazReporte : Form
+    {
+        public InterfazReporte()
+        {
+            InitializeComponent();
+        }
+
+        private void InterfazReporte_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_TituloCantidadDeContrasenias_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel7_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
