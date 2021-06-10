95aa6ec94be4d2f3dfcec379a714da46babf659b
SantiagoBolumburu76 <santiagobolumburu@hotmail.com>
2021-06-09 23:14:53 -0300

    TDD de la feature Encriptar Contrasenias, segunda fase (GREEN)

--- OblDiseño1/OblDiseño1/Manejadores/Encriptador.cs
+++ OblDiseño1/OblDiseño1/Manejadores/Encriptador.cs
@@ -5,26 +5,81 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using OblDiseño1.Interfaces;
+using System.IO;
 
 namespace OblDiseño1.Manejadores
 {
-    class Encriptador : IEncriptado
+    public class Encriptador : IEncriptado
     {
-        private byte[] Vector;
+        private readonly byte[] vectorDeInicializacion = new byte[16];
+
+        public Encriptador()
+        {
+
+        }
 
         public string Desencriptar(string contraseniaEncriptada, string llave)
         {
-            return null;
+            byte[] buffer = Convert.FromBase64String(contraseniaEncriptada);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Convert.FromBase64String(llave);
+                aes.IV = vectorDeInicializacion;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
         }
 
         public string Encriptar(string contrasenia, string llave)
         {
-            return null;
+            byte[] contraseniaEncriptada;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Convert.FromBase64String(llave);
+                aes.IV = vectorDeInicializacion;
+
+                ICryptoTransform encriptadorBasico = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encriptadorBasico, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(contrasenia);
+                        }
+
+                        contraseniaEncriptada = memoryStream.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(contraseniaEncriptada);
         }
+
 
         public string GenerarLlave()
         {
-            return null;
+            string llave;
+            using (Aes aes = Aes.Create())
+            {
+                aes.GenerateKey();
+                byte[] llaveEnBytes = aes.Key;
+                llave = Convert.ToBase64String(llaveEnBytes);
+            }
+            return llave;
         }
     }
 }
